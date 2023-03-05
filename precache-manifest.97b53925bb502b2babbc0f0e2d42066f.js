6aae781141e94648218fc3380c960eb938d8d27c
Ohleh <oleg.kolischuk@gmail.com>
2023-03-05 13:41:57 +0200

    Updates

---
+++ precache-manifest.97b53925bb502b2babbc0f0e2d42066f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d47dcb94b7d5d35c46c3",
+    "url": "/tweet-card/static/css/main.4f7750db.chunk.css"
+  },
+  {
+    "revision": "d47dcb94b7d5d35c46c3",
+    "url": "/tweet-card/static/js/main.d47dcb94.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/tweet-card/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "04bdd12f6c5350f5aa97",
+    "url": "/tweet-card/static/js/2.04bdd12f.chunk.js"
+  },
+  {
+    "revision": "7281ce98146fdf666336",
+    "url": "/tweet-card/static/js/runtime~main.7281ce98.js"
+  },
+  {
+    "revision": "44c45724455c9a18b429f1ff7f5c2a53",
+    "url": "/tweet-card/static/media/chat-m@2x.44c45724.png"
+  },
+  {
+    "revision": "1f2109d8181f016190e26a26d3090b33",
+    "url": "/tweet-card/static/media/avatar-m@2x.1f2109d8.png"
+  },
+  {
+    "revision": "ec557780edba1478763d5a1b2e169ccf",
+    "url": "/tweet-card/index.html"
+  }
+];
