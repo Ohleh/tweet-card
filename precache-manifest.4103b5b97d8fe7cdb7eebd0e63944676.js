4e8266dfc0542b22ab013d46df2c00a207af5872
Ohleh <oleg.kolischuk@gmail.com>
2023-04-30 19:33:49 +0300

    Updates

---
+++ precache-manifest.4103b5b97d8fe7cdb7eebd0e63944676.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a22d756d0fbd14843374",
+    "url": "/tweet-card/static/css/main.4f7750db.chunk.css"
+  },
+  {
+    "revision": "a22d756d0fbd14843374",
+    "url": "/tweet-card/static/js/main.a22d756d.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/tweet-card/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "04bdd12f6c5350f5aa97",
+    "url": "/tweet-card/static/js/2.04bdd12f.chunk.js"
+  },
+  {
+    "revision": "7281ce98146fdf666336",
+    "url": "/tweet-card/static/js/runtime~main.7281ce98.js"
+  },
+  {
+    "revision": "44c45724455c9a18b429f1ff7f5c2a53",
+    "url": "/tweet-card/static/media/chat-m@2x.44c45724.png"
+  },
+  {
+    "revision": "1f2109d8181f016190e26a26d3090b33",
+    "url": "/tweet-card/static/media/avatar-m@2x.1f2109d8.png"
+  },
+  {
+    "revision": "677450307a89c6380bd8880c5c4bf231",
+    "url": "/tweet-card/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tweet-card/precache-manifest.174d5aeae7e6ee3dfabe3ba781ed2146.js"
+  "/tweet-card/precache-manifest.4103b5b97d8fe7cdb7eebd0e63944676.js"
 );
 
 workbox.clientsClaim();
